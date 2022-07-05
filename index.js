53f20a997ebe776478ccacb19763c915e31c64d8
Jamon Holmgren <jamonholmgren@gmail.com>
2022-07-04 22:43:42 -0700

    Initial

---
+++ index.js
@@ -0,0 +1,11 @@
+console.log("");
+console.log("Hi, I'm Jamon!");
+console.log("");
+console.log("You really shouldn't ever run a CLI script like this.");
+console.log("If I wanted to, I could grab all your environment variables");
+console.log("and send them to myself.");
+console.log("I'm not a jerk, so I won't.");
+console.log("In the future, don't run untrusted scripts via npx.");
+console.log("");
+console.log("... with that said, feel free to visit https://jamon.dev!");
+console.log("");
